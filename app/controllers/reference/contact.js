af61b0f8f087734f1073ede21b8661fb8da8c9c2
Zelda <zelda.mki@outlook.com>
2022-06-10 18:40:23 +0700

    up

--- app/controllers/reference/contact.js
+++ app/controllers/reference/contact.js
@@ -1,41 +1,80 @@
+const models = require("../../models")
+const contact = models.Contact
 
 exports.list = (req, res) => {
-    let response = {
-        "data":{},
-        "message": "List Contact"
-    }
-    res.send(response);
+    contact.findAll({
+          order: [
+            ['id', 'DESC']
+          ]
+        }).then((data) => {
+            let response = {
+                "message": "Contact list successfully",
+                "data": data,
+            }
+            res.send(response);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
 }
 
 
 exports.create = (req, res) => {
-    let response = {
-        "data":{},
-        "message": "Create Contact"
-    }
-    res.send(response);
+    let formData = req.body
+    let newData = new contact(formData);
+    let result = newData.save();
+    result.then((data) => {
+        let response = {
+            "message": "Contact created successfully",
+            "data": data,
+        }
+        res.send(response);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
 }
 
 exports.update = (req, res) => {
-    let response = {
-        "data":{},
-        "message": "Update Contact"
-    }
-    res.send(response);
+    let id = req.params.id;
+    let formData = req.body;
+    let result = contact.update(formData, { where: { id: id } });
+    result.then((data) => {
+        let response = {
+            "message": "Contact udpated successfully",
+            "data": formData,
+        }
+        res.send(response);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
 }
 
 exports.detail = (req, res) => {
-    let response = {
-        "data":{},
-        "message": "Detail Contact"
-    }
-    res.send(response);
+    contact.findByPk(req.params.id).
+        then((data) => {
+             let response = {
+                "message": "Contact finded successfully",
+                "data": data,
+            }
+            res.send(response);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
 }
 
 exports.delete = (req, res) => {
-    let response = {
-        "data":{},
-        "message": "Delete Contact"
-    }
-    res.send(response);
+    let id = req.params.id
+    contact.destroy({ where: { id: id } }).
+        then((data) => {
+            res.status(200).json({
+                message: "Contact deleted successfully",
+                data: data
+            })
+        })
+        .catch((error) => {
+            console.log(error);
+        });
 }
